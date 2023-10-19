fa69a9fc22904eaecbd0091b7c5c928f95542ceb
Mohammad Sumon <mohammad.sumon9897@gmail.com>
2023-10-19 21:26:53 +0600

    Update CSS and JS

---
+++ includes/brands-slider.php
@@ -0,0 +1,73 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Brands</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/1.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/2.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/3.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/4.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/5.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/6.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/7.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/8.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/9.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/10.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+			
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
